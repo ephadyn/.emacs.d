89878c39752b06092ee204c490c01fc78f54ff5c
Hadyn Youens <hadyn@youens.nz>
2024-06-13 10:39:11 +1200

    Add language integration

---
+++ modules/crafted-languages-packages.el
@@ -0,0 +1,20 @@
+;;;; crafted-languages-packages.el --- Packages for programming languages  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024
+;; SPDX-License-Identifier: MIT
+
+;; Author: System Crafters Community
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(add-to-list 'package-selected-packages 'tide)
+(add-to-list 'package-selected-packages 'lsp-tailwindcss)
+(add-to-list 'package-selected-packages 'nix-mode)
+(add-to-list 'package-selected-packages 'sharper)
+
+(provide 'crafted-languages-packages)
+;;; crafted-languages-packages.el ends here
