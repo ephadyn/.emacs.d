4dcdeaa2403359d888c54e8bfc79ac6e10f2c858
Hadyn Youens <hadyn@youens.nz>
2024-06-13 11:10:15 +1200

    Set up treemacs

--- modules/crafted-ui-config.el
+++ modules/crafted-ui-config.el
@@ -176,5 +176,68 @@ Used as hook for modes which should not display line numebrs."
 (add-hook 'window-setup-hook #'crafted-setup-fonts)
 (add-hook 'server-after-make-frame-hook #'crafted-setup-fonts)
 
+;; Treemacs
+(when (require 'treemacs nil :noerror)
+  (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay        0.5
+          treemacs-directory-name-transformer      #'identity
+          treemacs-display-in-side-window          t
+          treemacs-eldoc-display                   'simple
+          treemacs-file-event-delay                2000
+          treemacs-file-extension-regex            treemacs-last-period-regex-value
+          treemacs-file-follow-delay               0.2
+          treemacs-file-name-transformer           #'identity
+          treemacs-follow-after-init               t
+          treemacs-expand-after-init               t
+          treemacs-find-workspace-method           'find-for-file-or-pick-first
+          treemacs-git-command-pipe                ""
+          treemacs-goto-tag-strategy               'refetch-index
+          treemacs-header-scroll-indicators        '(nil . "^^^^^^")
+          treemacs-hide-dot-git-directory          t
+          treemacs-indentation                     2
+          treemacs-indentation-string              " "
+          treemacs-is-never-other-window           nil
+          treemacs-max-git-entries                 5000
+          treemacs-missing-project-action          'ask
+          treemacs-move-files-by-mouse-dragging    t
+          treemacs-move-forward-on-expand          nil
+          treemacs-no-png-images                   nil
+          treemacs-no-delete-other-windows         t
+          treemacs-project-follow-cleanup          nil
+          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                        'right
+          treemacs-read-string-input               'from-child-frame
+          treemacs-recenter-distance               0.1
+          treemacs-recenter-after-file-follow      nil
+          treemacs-recenter-after-tag-follow       nil
+          treemacs-recenter-after-project-jump     'always
+          treemacs-recenter-after-project-expand   'on-distance
+          treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
+          treemacs-project-follow-into-home        nil
+          treemacs-show-cursor                     nil
+          treemacs-show-hidden-files               t
+          treemacs-silent-filewatch                nil
+          treemacs-silent-refresh                  nil
+          treemacs-sorting                         'alphabetic-asc
+          treemacs-select-when-already-in-treemacs 'move-back
+          treemacs-space-between-root-nodes        t
+          treemacs-tag-follow-cleanup              t
+          treemacs-tag-follow-delay                1.5
+          treemacs-text-scale                      nil
+          treemacs-user-mode-line-format           nil
+          treemacs-user-header-line-format         nil
+          treemacs-wide-toggle-width               70
+          treemacs-width                           45
+          treemacs-width-increment                 1
+          treemacs-width-is-initially-locked       t
+          treemacs-workspace-switch-cleanup        nil)
+  (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode 'always)
+    (treemacs-git-mode 'deferred)
+    (treemacs-git-commit-diff-mode t)
+    (treemacs-resize-icons 16)
+  )
+
 (provide 'crafted-ui-config)
 ;;; crafted-ui-config.el ends here
--- modules/crafted-ui-packages.el
+++ modules/crafted-ui-packages.el
@@ -16,6 +16,11 @@
 (add-to-list 'package-selected-packages 'helpful)
 (add-to-list 'package-selected-packages 'breadcrumb)
 (add-to-list 'package-selected-packages 'catppuccin-theme)
+(add-to-list 'package-selected-packages 'treemacs)
+(add-to-list 'package-selected-packages 'treemacs-evil)
+(add-to-list 'package-selected-packages 'treemacs-projectile)
+(add-to-list 'package-selected-packages 'treemacs-magit)
+(add-to-list 'package-selected-packages 'treemacs-all-the-icons)
 
 (provide 'crafted-ui-packages)
 ;;; crafted-ui-packages.el ends here
